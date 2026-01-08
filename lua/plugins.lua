8199f343a90c4c12038c540c1fe9cc8827531aac
Justin Wang <wangjpersonal@gmail.com>
2026-01-07 17:39:49 -0800

    rm flash, added cmt.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -375,21 +375,11 @@ require("lazy").setup({
 		},
 	},
 
-	-- Cursor navigation
+	-- Comment.nvim (gcc, gco, etc)
 	{
-		"folke/flash.nvim",
-		event = "VeryLazy",
-		---@type Flash.Config
-		opts = {},
-		keys = {
-			{
-				"s",
-				mode = { "n", "x", "o" },
-				function()
-					require("flash").jump()
-				end,
-				desc = "Flash",
-			},
+		"numToStr/Comment.nvim",
+		opts = {
+			-- add any options here
 		},
 	},
 
